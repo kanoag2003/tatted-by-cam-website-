51d4e1de2b602d088de6b9a5afb7e41dd32ab2c0
Kanoa Guillermo <kanoa.guillermo@gmail.com>
2026-01-28 21:35:19 -0800

    create a secret header on lambda

--- src/App.tsx
+++ src/App.tsx
@@ -126,7 +126,7 @@ function App() {
     const userData = { name, email, formattedDate, appointmentTime };
     try {
       // Get the URL data and convert to JSON
-    const response = await fetch('https://vrx2kxxqomkalbuehfkncwkdly0imcwk.lambda-url.us-west-2.on.aws/', { // Change to API gateway via lambda for production stage 
+    const response = await fetch('https://vrx2kxxqomkalbuehfkncwkdly0imcwk.lambda-url.us-west-2.on.aws/', { 
       method: 'POST',
       headers: { 'Content-Type' : 'application/json'},
       body: JSON.stringify(userData)
@@ -191,7 +191,7 @@ function App() {
       const userData = { cancelName, cancelEmail, formattedCancelDate, appointmentTime };
       try {
           // Get the URL data and convert to JSON
-        const response = await fetch('https://qdpptyrlzoapoxnimedhse7aoy0ascxm.lambda-url.us-west-2.on.aws/', { // Change to API gateway via lambda for production stage 
+        const response = await fetch('https://qdpptyrlzoapoxnimedhse7aoy0ascxm.lambda-url.us-west-2.on.aws/', { 
           method: 'DELETE',
           headers: { 'Content-Type' : 'application/json'},
           body: JSON.stringify(userData)
